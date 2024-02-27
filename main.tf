5c584e22670b38e99618e98e71f3039c2dcca3a8
Cesar Gutierrez Tineo <a20062161@pucp.edu.pe>
2024-02-27 14:02:27 -0500

    remove comments

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  region = "us-east-1" # Ejemplo: us-east-1
+  region = "us-east-1"
 }
 
 # Crear un repositorio de ECR
 resource "aws_ecr_repository" "mi_repositorio_ecr" {
   name                 = "challenge"
   image_tag_mutability = "MUTABLE"
-  
-  # Opcional: Configuración de políticas de ciclo de vida, etc.
 }
